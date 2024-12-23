8dbc8b8caaf61246c7384ba97334381076e42fab
Georgiy Doroshenko <Jostarex@gmail.com>
2024-12-23 08:03:35 +0300

    Practical Task L14 on Dec 18 2024

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,34 +1,32 @@
-import jdk.jfr.Description;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import java.util.stream.Stream;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import static org.example.Main.calculateFactorial;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertThrows;
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class FactorialTest {
 
-    @ParameterizedTest(name = "Result: {0}, Expected result: {1}")
-    @MethodSource("provideInputs")
+    @Test(description = "ParameterizedTest", dataProvider = "provideInputs")
     public void  factorialParameterizedTest(int input, int expected) {
         assertEquals(expected, calculateFactorial(input));
     }
 
     @Test
-    @Description("Check negative number")
-    public void factorialThrowExceptionTest() {
+    public void  factorialThrowsExceptionTest(){
         assertThrows(ArithmeticException.class, () -> calculateFactorial(-1));
     }
 
-    private static Stream<Arguments> provideInputs() {
-        return Stream.of(
-                Arguments.arguments(0, 1),
-                Arguments.arguments(1, 1),
-                Arguments.arguments(2, 2),
-                Arguments.arguments(3, 6),
-                Arguments.arguments(4, 24),
-                Arguments.arguments(5, 120),
-                Arguments.arguments(6, 720)
-        );
+    @DataProvider(name = "provideInputs")
+    private static Object[][] provideInputs() {
+        return new Object[][]{
+                {0, 1},
+                {1, 1},
+                {2, 2},
+                {3, 6},
+                {4, 24},
+                {5, 120},
+                {6, 720}
+        };
     }
 }
